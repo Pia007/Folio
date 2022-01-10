a2bf55220b2a0f62528b0734733074efdecbf85c
Pia <piatorain@yahoo.com>
2022-01-10 13:59:05 -0800

    Created component templates

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -3,17 +3,24 @@ import Nav from "./components/Nav";
 import Home from "./components/Home";
 import About from "./components/About";
 import Projects from "./components/Projects";
+import Contact from "./components/Contact";
 import Footer from "./components/Footer";
+
+import { Route, Switch, useHistory } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <Header />
       
+      <Header />
       <Nav />
-      <Home />
-      <About />
-      <Projects />
+      <Switch>
+        <Route exact path='/' component={Home}  />
+        <Route path='/About' component={About} />
+        <Route path='/Projects' component={Projects}/> 
+        <Route path='/Contact' component={Contact} />
+        
+      </Switch>
       <Footer />
       
     </div>
--- portfolio/src/components/Nav.js
+++ portfolio/src/components/Nav.js
@@ -1,7 +1,16 @@
+import { Link } from "react-router-dom";
+
 const Nav = () => {
     return (
         <nav>
             <h1>Nav</h1>
+            <ul>
+                <li><Link to="/"> Home</Link></li> 
+                <li><Link to="/About"> About</Link></li> 
+                <li><Link to="/Projects"> Projects</Link></li> 
+                <li><Link to="/Contact"> Contact</Link></li>
+                
+            </ul>
         </nav>
     )
 }
