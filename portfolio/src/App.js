962e2d47f873a610c3d59d852ce8f023e6697335
Pia <piatorain@yahoo.com>
2022-01-10 13:08:58 -0800

    First commit as a React App

---
+++ portfolio/src/App.js
@@ -0,0 +1,23 @@
+import Header from "./components/Header";
+import Nav from "./components/Nav";
+import Home from "./components/Home";
+import About from "./components/About";
+import Projects from "./components/Projects";
+import Footer from "./components/Footer";
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      
+      <Nav />
+      <Home />
+      <About />
+      <Projects />
+      <Footer />
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ portfolio/src/components/Header.js
@@ -0,0 +1,9 @@
+const Header = () => {
+    return (
+        <header>
+            <h1>Pia Torain</h1>
+        </header>
+    )
+}
+
+export default Header
