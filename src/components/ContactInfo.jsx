66d75b5435bcf949cc862b3ce2d5d90b96cccad6
Pia <piatorain@yahoo.com>
2022-04-30 12:04:50 -0700

    Replaced icons

--- src/components/ContactInfo.jsx
+++ src/components/ContactInfo.jsx
@@ -1,30 +1,34 @@
 import React from 'react';
 import { Row, Col } from "reactstrap";
+import Socials from "../components/Socials";
+import mailImg from '../images/mail.svg';
+import phoneImg from '../images/phone.svg';
 
 const ContactInfo = () => {
     return (
         <>
-            <Col  className="p-2">
+            <Col className="p-0 ">
                 <p className="contactText p-1 pb-0">
                     Reach out if you have any questions or just want to say, "Hi"! I am open to
                     discussing new projects and opportunities.
                 </p>
                 <Row className="mb-1">
-                    <Col className="d-flex">
+                    <Col className="d-flex my-1">
                         <p className="p-1 sm-py-0 mb-0">
-                            <i className="fa fa-envelope-o fa-2x text-primary envelope"/> 
+                            <img src={mailImg} alt="envelope" /> 
                         </p>
-                        <a className="p-1 mb-md-1 contactText" href="mailto:info@hostahealthcareheroes.org"> piatorain@yahoo.com </a>
+                        <a className="p-1 mb-md-1 contactText" href="mailto:info@hostahealthcareheroes.org"> piatorain@gmail.com </a>
                     </Col>
                 </Row>
                 <Row className="">
-                    <Col className="d-flex">
+                    <Col className="d-flex mb-1">
                         <p className="p-1 sm-py-0 mb-0">
-                            <i className="fa fa-phone fa-2x mr-2 text-primary phone"/>
+                            <img src={phoneImg} alt="phone" /> 
                         </p>
-                        <a role="button" className="px-2 py-1 mb-md-1 contactText text-decoration-none" href="tel:+19084005335" >(908) 400-5335</a> 
+                        <a role="button" className="p-1 mb-md-1 contactText text-decoration-none" href="tel:+19084005335" >(908) 400-5335</a> 
                     </Col>
                 </Row>
+                <Socials />
             </Col>
         </>
     );
