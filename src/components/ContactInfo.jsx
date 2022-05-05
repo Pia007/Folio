ea3e1049018c45a2bcda4710bbf8356caf7e9d11
Pia <piatorain@yahoo.com>
2022-05-05 11:17:20 -0700

    Edits

--- src/components/ContactInfo.jsx
+++ src/components/ContactInfo.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Row, Col } from "reactstrap";
-import Socials from "../components/Socials";
 import mailImg from '../images/mail.svg';
 import phoneImg from '../images/phone.svg';
 import pinImg from '../images/location.svg';
@@ -9,7 +8,7 @@ const ContactInfo = () => {
     return (
         <>
             <Col className="p-0 ">
-                <p className="contactText p-1 pb-0">
+                <p className="contact-text p-1 pb-0">
                     Reach out if you have any questions or just want to say, "Hi"! I am open to
                     discussing new projects and opportunities.
                 </p>
@@ -18,7 +17,7 @@ const ContactInfo = () => {
                         <p className="p-1 sm-py-0 mb-0">
                             <img src={mailImg} alt="envelope" /> 
                         </p>
-                        <a className="p-1 mb-md-1 contactText" href="mailto:info@hostahealthcareheroes.org"> piatorain@gmail.com </a>
+                        <a className="p-1 mb-md-1 contact-text" href="mailto:info@hostahealthcareheroes.org"> piatorain@gmail.com </a>
                     </Col>
                 </Row>
                 <Row className="">
@@ -26,7 +25,7 @@ const ContactInfo = () => {
                         <p className="p-1 sm-py-0 mb-0">
                             <img src={phoneImg} alt="phone" /> 
                         </p>
-                        <a role="button" className="p-1 mb-md-1 contactText text-decoration-none" href="tel:+19084005335" >(908) 400-5335</a> 
+                        <a role="button" className="p-1 mb-md-1 contact-text text-decoration-none" href="tel:+19084005335" >(908) 400-5335</a> 
                     </Col>
                 </Row>
                 <Row className="">
@@ -34,10 +33,9 @@ const ContactInfo = () => {
                         <p className="p-0 px-1 mb-0">
                             <img src={pinImg} alt="location" /> 
                         </p>
-                        <p className='p-1'> Las Vegas Metropolitan Area</p>
+                        <p className='p-1 contact-text'> Las Vegas Metropolitan Area</p>
                     </Col>
                 </Row>
-                {/* <Socials /> */}
             </Col>
         </>
     );
