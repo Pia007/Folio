74e44edf164790c511fc7fe477655bdcda08ec36
Pia <piatorain@yahoo.com>
2022-05-05 17:37:00 -0700

    added to projects header

---
+++ src/components/Current.jsx
@@ -0,0 +1,14 @@
+import { Row } from 'reactstrap';
+
+const Current = () => {
+    return (
+        <>
+            <Row>
+                <h2 className='project-text'>Currently Working On?</h2>
+                <h3 className='text'> I am currently working on improving my <span className='text-primary'>JavaScript</span> skills and building <span className='text-primary'>Fullstack Web Applications</span>.</h3>
+            </Row>
+        </>
+    )
+}
+
+export default Current
