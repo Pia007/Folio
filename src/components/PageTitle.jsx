343bcd4616529f091fb7cbeb7a4f7691d57de310
Pia <piatorain@yahoo.com>
2022-05-09 14:57:16 -0700

    title margin

--- src/components/PageTitle.jsx
+++ src/components/PageTitle.jsx
@@ -5,7 +5,7 @@ function PageTitle({ regText, colorText }) {
     return (
         <Row>
             <Zoom delay={500}>
-                <h1 className='text-center my-0 py-2'><span className='title'>{regText}</span><span className='text-primary title'> {colorText}</span></h1>
+                <h1 className='text-center my-0 mt-1 py-2'><span className='title'>{regText}</span><span className='text-primary title'> {colorText}</span></h1>
             </Zoom>
         </Row>
     );
