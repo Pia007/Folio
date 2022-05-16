f6fe741b0e0e54c79c282bea4622a792d967c0c3
Pia <piatorain@yahoo.com>
2022-05-16 16:37:54 -0700

    footer and vh

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,6 +2,7 @@ import Zoom from 'react-reveal/Zoom';
 
 
 export const DesignColumn = () => {
+    
     return ( 
         <div className='d-none d-md-block d-flex align-content-end position-fixed' style={{ width:'3%', height: 'auto', bottom: '10px', right: '0'}}>
             <div className='p-0 mb-0 d-flex justify-content-around align-content-end sticky-bottom'>
@@ -14,10 +15,11 @@ export const DesignColumn = () => {
 }
 
 const Footer = () => {
+    const today = new Date();
     return (
         <div className='w-100 d-md-none' style={{ position: 'fixed', bottom: '0', fontSize:'.875em'}}>
             <Zoom delay={2500}>
-                <p className='text-center footer-text'>Designed and Built by Pia Torain</p>
+                <p className='text-center footer-text'>Copyright &copy; {today.getFullYear()} Designed and Built by Pia Torain</p>
             </Zoom>
         </div>
     )
